5b6fa243a1711857fab212d48c7e69e0a4ef13d4
Mirun Kaushik <10425mirunkaushik@gmail.com>
2025-07-11 15:48:29 +0530

    first commit

---
+++ bot/debateJudge.js
@@ -0,0 +1,37 @@
+import { InferenceClient } from "@huggingface/inference";
+
+const debatJudge = async (arguement) =>{
+  const client = new InferenceClient('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');
+
+  const chatCompletion = await client.chatCompletion({
+  provider: "featherless-ai",
+  model: "Qwen/Qwen2-7B-Instruct",
+  messages: [
+    {
+      role: "user",
+      content: `
+You are a judge. Score this argument on a scale of 0 to 10 for the following criteria:
+- Relevance
+- Logic
+- Fallacy (state the name or "None")
+- Clarity
+- Evidence
+- Persuasiveness
+- Originality
+
+Debate Topic:
+"AI should not be allowed to make decisions in life-or-death situations."
+
+Argument:
+${arguement}
+
+return Score=(Relevance*0.15+Logic*0.20+Fallacy*0.15+Clarity*0.10+Evidence*0.15+Persuasiveness*0.15+Originality*0.10).
+      `,
+    },
+  ],
+});
+
+return(chatCompletion.choices[0].message.content);
+}
+
+export default debatJudge
---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import debatJudge from '../bot/debateJudge.js';
+
+const App = () => {
+  const [input,setInput] = useState("");
+  const [result,setResult] = useState("")
+  
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    const r = await debatJudge(input);
+    setResult(r)
+    
+  }
+
+
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="" id="" className='w-40 h-10 bg-amber-400' value={input} onChange={(e)=> setInput(e.target.value)}/>
+        <button className='border-2'>submit</button>
+      </form>
+      <div>
+      {result && <prev>{result}</prev>}
+      </div>
+    </div>
+  )
+}
+
+export default App
