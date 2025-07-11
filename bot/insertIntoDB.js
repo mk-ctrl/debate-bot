b4e6f284f0f5fbba0beff0cd7e8c648a402749e6
Mirun Kaushik <10425mirunkaushik@gmail.com>
2025-07-11 15:59:31 +0530

    3rd commit

--- bot/insertIntoDB.js
+++ bot/insertIntoDB.js
@@ -1,34 +1,34 @@
-// This File is used to connect with the mongoDB database and activate the trigger function
-// for embedding the inserted data. The 1st and most important step
+// // This File is used to connect with the mongoDB database and activate the trigger function
+// // for embedding the inserted data. The 1st and most important step
 
-import { MongoClient } from "mongodb";
-const client = new MongoClient('mongodb+srv://mirunkaushik:mirun2005@cluster0.g8yoz.mongodb.net/');
+// import { MongoClient } from "mongodb";
+// const client = new MongoClient('mongodb+srv://mirunkaushik:mirun2005@cluster0.g8yoz.mongodb.net/');
 
-const connectDB = async () => {
-    try {
-        await client.connect();
-        console.log("DataBase Connected Successfully");
+// const connectDB = async () => {
+//     try {
+//         await client.connect();
+//         console.log("DataBase Connected Successfully");
         
-    } catch (error) {
-        console.error(error)
-    }
-}
+//     } catch (error) {
+//         console.error(error)
+//     }
+// }
 
-connectDB();
+// connectDB();
 
-//Procedure for CRUD operation in db
-const db = client.db('Debate');
-const collection = db.collection('room1');
+// //Procedure for CRUD operation in db
+// const db = client.db('Debate');
+// const collection = db.collection('room1');
 
-//function to peform insertion
+// //function to peform insertion
 
-const Insert = async ()=>{
-    const document = {
-        'Title' : 'AI should not be allowed to make decisions in life-or-death situations.'
-    }
+// const Insert = async ()=>{
+//     const document = {
+//         'Title' : 'AI should not be allowed to make decisions in life-or-death situations.'
+//     }
 
-    await collection.insertOne(document);
-    console.log('Inserted Successfully');
-}
+//     await collection.insertOne(document);
+//     console.log('Inserted Successfully');
+// }
 
-Insert();
+// Insert();
